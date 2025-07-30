cc6b8b70e2dc9289216d91dbb1d803e9c9b636e6
Fyrna N. <imfyrna@gmail.com>
2025-07-30 17:53:51 +0700

    chore: enhancement for Args()

--- args.go
+++ args.go
@@ -1,15 +1,24 @@
 package cli
 
+import "strings"
+
 type Args []string
 
+// returns slice of Args
 func (c *Context) Args() Args {
 	return Args(c.Flags.Args())
 }
 
+// get spesific arg by index
 func (a Args) Get(i int) string {
 	if i < 0 || i >= len(a) {
 		return ""
 	}
 
 	return a[i]
+}
+
+// returns all args
+func (a Args) All() string {
+	return strings.Join(a, " ")
 }
